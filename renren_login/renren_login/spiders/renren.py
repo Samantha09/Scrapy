041e6575f88ee4be86d26727adf40383f72bd573
Samantha09 <zsr2541@163.com>
2019-03-30 16:05:05 +0800

    v3

---
+++ renren_login/renren_login/spiders/renren.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class RenrenSpider(scrapy.Spider):
+    name = 'renren'
+    allowed_domains = ['renren.com']
+    start_urls = ['http://renren.com/']
+
+    def start_requests(self):
+        url = "http://www.renren.com/PLogin.do"
+
+        data = {
+            "email": '15692036272',
+            "password": "951218/lucky",
+        }
+        request = scrapy.FormRequest(url, formdata=data, callback=self.parse_page)
+        yield request
+
+    def parse_page(self, response):
+        with open('renren.html', 'w', encoding='utf8') as fp:
+            fp.write(response.text)
