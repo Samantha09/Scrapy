5f47ee934dbd90681be2e892f195e2d16da981d3
Samantha09 <zsr2541@163.com>
2019-03-30 18:02:17 +0800

    v5

--- douban_login/douban_login/spiders/douban.py
+++ douban_login/douban_login/spiders/douban.py
@@ -5,7 +5,7 @@ import scrapy
 class DoubanSpider(scrapy.Spider):
     name = 'douban'
     allowed_domains = ['douban.com']
-    start_urls = ['http://douban.com/']
+    start_urls = ['https://accounts.douban.com/passport/login']
 
     def parse(self, response):
         pass
