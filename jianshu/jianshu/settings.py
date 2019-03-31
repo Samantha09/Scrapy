5483c80695128fc3ae848f6baa84447d2996fe9a
Samantha09 <zsr2541@163.com>
2019-04-01 06:42:53 +0800

    V8

--- jianshu/jianshu/settings.py
+++ jianshu/jianshu/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'jianshu.spiders'
 #USER_AGENT = 'jianshu (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -39,10 +39,11 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Accept-Language': 'en',
+  'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+}
 
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
@@ -52,9 +53,9 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'jianshu.middlewares.JianshuDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
@@ -64,9 +65,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'jianshu.pipelines.JianshuPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   # 'jianshu.pipelines.JianshuPipeline': 300,
+  'jianshu.pipelines.JianshuTwistedPipeline': 300,
+
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
--- jianshu/jianshu/spiders/js.py
+++ jianshu/jianshu/spiders/js.py
@@ -2,7 +2,8 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
+import re
+from jianshu.items import ArticleItem
 
 class JsSpider(CrawlSpider):
     name = 'js'
@@ -10,12 +11,26 @@ class JsSpider(CrawlSpider):
     start_urls = ['http://jianshu.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'.*/p/[0-9a-z].*'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        title = response.xpath("//h1[@class='title']/text()").get()
+        avatar = response.xpath("//a[@class='avatar']/img/@src").get()
+        author = response.xpath("//span[@class='name']/a/text()").get()
+        pub_time = response.xpath("//span[@class='publish-time']/text()").get()
+        url = response.url
+        article_id = re.match(r'.*?/p/([0-9a-z]{12})[\?]*.*?', response.url).group(1)
+        content = response.xpath("//div[@class='show-content']").get()
+        content = re.sub(r"<.*?>", "", content).strip()
+
+        item = ArticleItem(
+            title=title,
+            avatar=avatar,
+            author=author,
+            origin_url=url,
+            pub_time=pub_time,
+            article_id=article_id,
+            content=content
+        )
+        yield item
